2b26f37d1284f5aa6b545c66f07927c6d0ea67dc
Osmar de Oliveira Braz Junior <osmarbraz@hotmail.com>
2023-04-11 14:35:08 -0300

    Início do projeto

---
+++ README.md
@@ -0,0 +1,6 @@
+# Cliente de Chat usando 1 Thread e Java Datagrama.
+
+- Programa cliente do chat usando java datagrama.
+- Permite conectar a um servidor.
+- Não recebe mensagens do servidor.
+- Use o repositório "chat_servidor_1_thread_datagrama" como o programa servidor.
---
+++ src/main/java/Principal.java
@@ -0,0 +1,28 @@
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import javax.swing.JOptionPane;
+
+public class Principal {
+
+    public static void main(String args[]) {
+        try {
+            DatagramSocket con = new DatagramSocket();//cria o objeto
+            System.out.println("Inicio cliente");
+            String mensagem = "";
+            while (!mensagem.equals("fim")) {
+                mensagem = JOptionPane.showInputDialog("Digite a Mensagem ou fim para sair");
+                //Converte a mensagem em um vetor de byte
+                byte dado[] = mensagem.getBytes();
+                //Configura o pacote
+                DatagramPacket pacote = new DatagramPacket(dado, dado.length, InetAddress.getByName("localhost"), 4444);
+                //envia o pacote
+                con.send(pacote);
+            }
+        } catch (IOException io) {
+            System.err.println("Problemas de IO");
+        }
+    }
+}
